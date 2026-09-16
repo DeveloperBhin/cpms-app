e6b90db34a89001a06a7ba869cb2eabd574cfb46
Norbeth <norbeth559@gmail.com>
2026-09-16 15:27:36 +0300

    apps

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -12,53 +12,120 @@ class BottomNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: onTap,
-
-      type: BottomNavigationBarType.fixed,
-
-      backgroundColor: Colors.white,
-
-      selectedItemColor: Colors.green,
-      unselectedItemColor: Colors.grey,
-
-      selectedFontSize: 11,
-      unselectedFontSize: 10,
-
-      elevation: 8,
-
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home_outlined),
-          activeIcon: Icon(Icons.home),
-          label: 'Home',
+    return SafeArea(
+      minimum: const EdgeInsets.fromLTRB(
+        12,
+        0,
+        12,
+        10,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(22),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(
+                alpha: 0.08,
+              ),
+              blurRadius: 12,
+              offset: const Offset(0, 3),
+            ),
+          ],
         ),
+        clipBehavior: Clip.antiAlias,
+        child: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: onTap,
 
-        BottomNavigationBarItem(
-          icon: Icon(Icons.crop_square_outlined),
-          activeIcon: Icon(Icons.crop_square),
-          label: 'Farms',
+          type: BottomNavigationBarType.fixed,
+
+          backgroundColor: Colors.white,
+
+          selectedItemColor:
+              const Color(0xFF087A2F),
+
+          unselectedItemColor:
+              const Color(0xFF9AA39D),
+
+          selectedFontSize: 10,
+          unselectedFontSize: 9,
+
+          selectedLabelStyle:
+              const TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+
+          unselectedLabelStyle:
+              const TextStyle(
+            fontWeight: FontWeight.w500,
+          ),
+
+          elevation: 0,
+
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_outlined,
+                size: 20,
+              ),
+              activeIcon: Icon(
+                Icons.home,
+                size: 20,
+              ),
+              label: 'Home',
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.crop_square_outlined,
+                size: 20,
+              ),
+              activeIcon: Icon(
+                Icons.crop_square,
+                size: 20,
+              ),
+              label: 'Farms',
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.park_outlined,
+                size: 20,
+              ),
+              activeIcon: Icon(
+                Icons.park,
+                size: 20,
+              ),
+              label: 'Trees',
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.assignment_outlined,
+                size: 20,
+              ),
+              activeIcon: Icon(
+                Icons.assignment,
+                size: 20,
+              ),
+              label: 'Activities',
+            ),
+
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.menu_outlined,
+                size: 20,
+              ),
+              activeIcon: Icon(
+                Icons.menu,
+                size: 20,
+              ),
+              label: 'More',
+            ),
+          ],
         ),
-
-        BottomNavigationBarItem(
-          icon: Icon(Icons.park_outlined),
-          activeIcon: Icon(Icons.park),
-          label: 'Trees',
-        ),
-
-        BottomNavigationBarItem(
-  icon: Icon(Icons.assignment_outlined),
-  activeIcon: Icon(Icons.assignment),
-  label: 'Activities',
-),
-
-        BottomNavigationBarItem(
-          icon: Icon(Icons.menu_outlined),
-          activeIcon: Icon(Icons.menu),
-          label: 'More',
-        ),
-      ],
+      ),
     );
   }
 }
